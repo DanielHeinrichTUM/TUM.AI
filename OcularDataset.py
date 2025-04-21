9e9e7f2151939b9f42605eb1d011ea332e65e753
Daniel <111065054+DanielHeinrichTUM@users.noreply.github.com>
2025-04-21 19:59:38 +0200

    more progress, can start training soon, just got some little kinks here and there. Sadly not the good ones

--- OcularDataset.py
+++ OcularDataset.py
@@ -3,48 +3,40 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
 import numpy as np
+import os
 
 class OcularDataset(Dataset):
-    def __init__(self, input_df, truth_df, transform=None):
-        self.input_df = input_df
-        self.truth_df = truth_df
+    def __init__(self, df, image_dir, transform=None):
+        self.df = df
+        self.image_dir = image_dir
         self.transform = transform or transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                               std=[0.229, 0.224, 0.225])
+                                std=[0.229, 0.224, 0.225])
         ])
-        
+        self.sex_mapping = {"Male": 0, "Female": 1}
+
     def __len__(self):
-        return len(self.input_df)
+        return len(self.df)
     
     def __getitem__(self, idx):
-        # Get image paths (using right fundus as example)
-        left_img_path = self.input_df.iloc[idx]["Left-Fundus"]
-        right_img_path = self.input_df.iloc[idx]["Right-Fundus"]
+        row = self.df.iloc[idx]
         
-        # Load images
-        left_img = Image.open(left_img_path).convert('RGB')
-        right_img = Image.open(right_img_path).convert('RGB')
+        left_img = self._load_image(row["Left-Fundus"])
+        right_img = self._load_image(row["Right-Fundus"])
         
-        # Apply transforms
-        left_img = self.transform(left_img)
-        right_img = self.transform(right_img)
-        
-        # Stack images (assuming you want to use both eyes)
-        images = torch.stack([left_img, right_img])
-        
-        # Get labels (convert to float32 for BCE loss)
-        labels = torch.FloatTensor(
-            self.truth_df.iloc[idx][["N","D","G","C","A","H","M","O"]].values
-        )
-        
-        # Get additional metadata if needed
-        sex = 0 if self.input_df.iloc[idx]["Patient Sex"] == "Male" else 1
+        labels = row[["N","D","G","C","A","H","M","O"]].values.astype(float)
+        sex = self.sex_mapping.get(row["Patient Sex"], 0) #defaulting to male if unknown, just like in real medical research
         
         return {
-            "images": images,          # Shape: [2, 3, 224, 224]
-            "labels": labels,          # Shape: [8]
-            "sex": torch.tensor(sex),  # 0/1
-            "id": self.input_df.iloc[idx]["ID"]
+            "images": torch.stack([left_img, right_img]),
+            "labels": torch.FloatTensor(labels),
+            "sex": torch.tensor(sex),
+            "id": row["ID"]
         }
+    
+    def _load_image(self, filename):
+        img_path = os.path.join(self.image_dir, filename)
+        img = Image.open(img_path).convert('RGB')
+        return self.transform(img)
